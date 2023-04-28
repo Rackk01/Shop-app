df8ec79248be3a72626db07f44233baec9a8f69b
Facu_Diaz <108439711+Rackk01@users.noreply.github.com>
2023-04-28 12:42:54 -0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,87 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8fcef1d81b6ae4046869a7d40f1018d7f7ac8133',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8fcef1d81b6ae4046869a7d40f1018d7f7ac8133',
+    ),
+    'doctrine/annotations' => 
+    array (
+      'pretty_version' => '1.13.3',
+      'version' => '1.13.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '648b0343343565c4a056bfc8392201385e8d89f0',
+    ),
+    'doctrine/common' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e09556bbdf95b8420e649162b19ae9da2d1a80f3',
+    ),
+    'doctrine/event-manager' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eb2ecf80e3093e8f3c2769ac838e27d8ede8e683',
+    ),
+    'doctrine/lexer' => 
+    array (
+      'pretty_version' => '1.2.3',
+      'version' => '1.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c268e882d4dbdd85e36e4ad69e02dc284f89d229',
+    ),
+    'doctrine/persistence' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ac6fce61f037d7e54dbb2435f5b5648d86548e23',
+    ),
+    'mercadopago/dx-php' => 
+    array (
+      'pretty_version' => '2.4.9',
+      'version' => '2.4.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8389608ab859e5f9394910ec1bd74d603d7c17a6',
+    ),
+    'psr/cache' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aa5030cfa5405eccfdcb1083ce040c2cb8d253bf',
+    ),
+  ),
+);
