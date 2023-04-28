19a3e110f847dc99dfa1563c43e3f3f3741a3251
Facu_Diaz <108439711+Rackk01@users.noreply.github.com>
2023-04-28 18:01:05 -0300

    add php folder

---
+++ php/constants.php
@@ -0,0 +1,17 @@
+<?php
+
+// A) Referido al service backend, en producción siempre corresponde que esté descomentada
+define('URL_BACKEND', 'http://localhost/Trabajos/santiago-backend-v2.1/'); // -- LOCAL
+// define('URL_BACKEND', 'https://hls.com.ar/carrito-backend/'); // -- SERVER
+
+//-----------------------------------------------------------------------------------------
+
+// B) Referido a la url en la que el sistema se implementa
+define('URL_APP', 'http://localhost/Trabajos/santiago-app/'); // -- LOCAL
+// define('URL_APP', 'https://test.carrito.hls.com.ar/'); // -- SERVER
+
+// C) Base de datos con la que trabaja
+define('DB_APP', 'santiago_app');
+// define('DB_APP', 'maxi_carrito_compras');
+
+//-----------------------------------------------------------------------------------------
